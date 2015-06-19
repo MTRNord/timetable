e4845f37132f2de207014e5388e256f34c305455
Marcel <MTRNord@users.noreply.github.com>
2015-06-19 12:16:33 +0200

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var sys = require("sys"),
-   http = require("http");
-http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Worldn");
-}).listen(8000);
-sys.puts("Server running at Port 8000");
